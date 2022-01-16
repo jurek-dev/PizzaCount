a0e0de928d258796360f92744aba2ebca6c02fd4
jurek-dev <jurek.dev@outlook.com>
2022-01-16 11:42:39 -0300

    Alteração para projeto To-do List

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+const form = document.getElementById("task-form"); // form recebe o ID do formulário da página
+const TaskList = document.getElementById("tasks"); // TaskList recebe o ID da DIV da página.
+
+// Ao submeter o formulário:
+form.onsubmit = function (e) {
+    e.preventDefault();
+    const inputField = document.getElementById('task-input');
+    addTask(inputField.value);
+    form.reset();
+};
+
+function addTask(description) {
+    // Criação dos elementos definidas nas variáveis:
+    const taskContainer = document.createElement('div');
+    const newTask = document.createElement('input');
+    const taskLabel = document.createElement('label');
+    const taskDescriptionNode = document.createTextNode(description);
+
+    newTask.setAttribute('type', 'checkbox');
+    newTask.setAttribute('name', description);
+    newTask.setAttribute('id', description);
+
+    taskLabel.setAttribute('for', description);
+    taskLabel.appendChild(taskDescriptionNode);
+
+    taskContainer.classList.add('task-item');
+    taskContainer.appendChild(newTask);
+    taskContainer.appendChild(taskLabel);
+
+    TaskList.appendChild(taskContainer);
+}
